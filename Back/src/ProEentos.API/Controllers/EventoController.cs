2069d37032891b394d41406dbd538f7b81ca7f6a
vini <vinicius.politta2@gmail.com>
2022-09-30 14:02:42 -0300

    created event controllers

---
+++ Back/src/ProEentos.API/Controllers/EventoController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEentos.API.Models;
+
+namespace ProEentos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento =  new Evento[]{
+            new Evento() {
+                EventoId = 1,
+                Tema = "Angular",
+                Local = "Belo Horizonte",
+                Lote = "1 Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemURL = "teste.jpg"
+            },
+            
+            new Evento() {
+                EventoId = 2,
+                Tema = ".NET",
+                Local = "Sao Paulo",
+                Lote = "1 Lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(3).ToString(),
+                ImagemURL = "teste1.jpg"
+            }
+           };
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Ex de post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Ex de put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Ex de Del com id = {id}";
+        }
+    }
+}
